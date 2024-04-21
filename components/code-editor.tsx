876ea3ead792edfa60e84263f9ee6ecc2787d475
ido evergeen <dicksonevergreenido@gmail.com>
2024-04-21 04:44:44 +0100

    add new logo

--- components/code-editor.tsx
+++ components/code-editor.tsx
@@ -2,28 +2,43 @@
 import React, { useRef } from "react";
 
 import { UnControlled as CodeMirror } from "react-codemirror2";
+
+// import { EditorView } from "codemirror/view";
 require("codemirror/mode/xml/xml");
 require("codemirror/mode/javascript/javascript");
 
 export default function CodeEditor() {
+  const value = `const pluckDeep = key => obj => key.split('.').reduce((accum, key) => accum[key], obj)
+
+  const compose = (...fns) => res => fns.reduce((accum, next) => next(accum), res)
+  
+  const unfold = (f, seed) => {
+    const go = (f, seed, acc) => {
+      const res = f(seed)
+      return res ? go(f, res[1], acc.concat([res[0]])) : acc
+    }
+    return go(f, seed, [])
+  }`;
   return (
     <div className="border-[3px] border-white rounded-lg p-4 w-full h-full relative lg:w-[50%]">
       <div className=" relative">
-        <div className="bg-red-300 w-full h-full z-0    "
-        
-        style={{
-          padding:"56px 56px"
-        }}
+        <div
+          className="bg-[#4F46E5] w-full h-full z-0    "
+          style={{
+            padding: "56px 56px",
+          }}
         >
-          <div className=" ">
+          <div className=" overflow-hidden ">
             <CodeMirror
-              value="<h1>I ♥ react-codemirror2</h1>"
+              value={value}
               options={{
                 mode: "javascript",
                 theme: "material",
+                viewportMargin: Infinity,
+
                 // lineNumbers: true,
               }}
-              className="z-50  "
+              className="z-50  text-sm pl-3 overflow-hidden "
               onChange={(editor, data, value) => {}}
             />
           </div>
